81fc85d9f8ee5f304f930c1a866553dab8b38104
Sandhya <neupanesandhya4567@gamil.com>
2023-06-27 17:19:30 +0545

    second commit

--- src/component/Sdata.jsx
+++ src/component/Sdata.jsx
@@ -3,7 +3,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/54/90/5490cb32786d4f7fef0f40d7266df532/5490cb32786d4f7fef0f40d7266df532.jpg",
     title: "One Piece",
-    text: "This shows the love between  binod and hinata.",
+    text: "Gold Roger was known as the Pirate King, the strongest..",
     time: "TV.23 min",
     link: "https://zoro.to/one-piece-100",
   },
@@ -12,15 +12,15 @@ const Sdata = [
       "https://img.zorores.com/_r/300x400/100/9c/bc/9cbcf87f54194742e7686119089478f8/9cbcf87f54194742e7686119089478f8.jpg",
 
     title: "Naruto:Shippuden",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "It has been two and a half years since Naruto Uzumaki left",
     time: "TV.16 min",
     link: "https://zoro.to/naruto-shippuden-355",
   },
   {
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",
-    title: "Demon Slayer: Kimetsu no ....",
-    text: "This shows the love between  binod and hinata.",
+    title: "Demon Slayer",
+    text: "It adapts the story from volume 12 (chapter 98 onwards) of the manga series",
     time: "TV.30min",
     link: "https://zoro.to/demon-slayer-kimetsu-no-yaiba-swordsmith-village-arc-18056",
   },
@@ -28,7 +28,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",
     title: "Black Clover",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "Asta and Yuno were abandoned at the same church on the same day",
     time: "TV.25 min",
     link: "https://zoro.to/black-clover-2404",
     link: "https://zoro.to/black-clover-2404",
@@ -37,7 +37,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/54/90/5490cb32786d4f7fef0f40d7266df532/5490cb32786d4f7fef0f40d7266df532.jpg",
     title: "One Piece",
-    text: "This shows the love between  binod and hinata.",
+    text: "Gold Roger was known as the Pirate King, the strongest..",
     time: "TV.23 min",
     link: "https://zoro.to/one-piece-100",
   },
@@ -46,15 +46,15 @@ const Sdata = [
       "https://img.zorores.com/_r/300x400/100/9c/bc/9cbcf87f54194742e7686119089478f8/9cbcf87f54194742e7686119089478f8.jpg",
 
     title: "Naruto:Shippuden",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "It has been two and a half years since Naruto Uzumaki left ",
     time: "TV.16 min",
     link: "https://zoro.to/naruto-shippuden-355",
   },
   {
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",
-    title: "Demon Slayer: Kimetsu no ....",
-    text: "This shows the love between  binod and hinata.",
+    title: "Demon Slayer",
+    text: "It adapts the story from volume 12 (chapter 98 onwards) of the manga series",
     time: "TV.30min",
     link:'https://zoro.to/demon-slayer-kimetsu-no-yaiba-swordsmith-village-arc-18056'
   },
@@ -62,7 +62,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",
     title: "Black Clover",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "Asta and Yuno were abandoned at the same church on the same day",
     time: "TV.25 min",
     link: "https://zoro.to/black-clover-2404",
   },
@@ -70,7 +70,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/54/90/5490cb32786d4f7fef0f40d7266df532/5490cb32786d4f7fef0f40d7266df532.jpg",
     title: "One Piece",
-    text: "This shows the love between  binod and hinata.",
+    text: "Gold Roger was known as the Pirate King, the strongest..",
     time: "TV.23 min",
     link: "https://zoro.to/one-piece-100",
   },
@@ -79,15 +79,15 @@ const Sdata = [
       "https://img.zorores.com/_r/300x400/100/9c/bc/9cbcf87f54194742e7686119089478f8/9cbcf87f54194742e7686119089478f8.jpg",
 
     title: "Naruto:Shippuden",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "It has been two and a half years since Naruto Uzumaki left ",
     time: "TV.16 min",
     link: "https://zoro.to/naruto-shippuden-355",
   },
   {
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",
-    title: "Demon Slayer: Kimetsu no ....",
-    text: "This shows the love between  binod and hinata.",
+    title: "Demon Slayer",
+    text:  "It adapts the story from volume 12 (chapter 98 onwards) of the manga series",
     time: "TV.30min",
     link:'https://zoro.to/demon-slayer-kimetsu-no-yaiba-swordsmith-village-arc-18056'
   },
@@ -95,7 +95,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",
     title: "Black Clover",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "Asta and Yuno were abandoned at the same church on the same day",
     time: "TV.25 min",
     link: "https://zoro.to/black-clover-2404",
   },
@@ -103,7 +103,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/54/90/5490cb32786d4f7fef0f40d7266df532/5490cb32786d4f7fef0f40d7266df532.jpg",
     title: "One Piece",
-    text: "This shows the love between  binod and hinata.",
+    text: "Gold Roger was known as the Pirate King, the strongest..",
     time: "TV.23 min",
     link: "https://zoro.to/one-piece-100",
   },
@@ -112,15 +112,15 @@ const Sdata = [
       "https://img.zorores.com/_r/300x400/100/9c/bc/9cbcf87f54194742e7686119089478f8/9cbcf87f54194742e7686119089478f8.jpg",
 
     title: "Naruto:Shippuden",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "It has been two and a half years since Naruto Uzumaki left ",
     time: "TV.16 min",
     link: "https://zoro.to/naruto-shippuden-355",
   },
   {
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",
-    title: "Demon Slayer: Kimetsu no ....",
-    text: "This shows the love between  binod and hinata.",
+    title: "Demon Slayer",
+    text:  "It adapts the story from volume 12 (chapter 98 onwards) of the manga series",
     time: "TV.30min",
     link:'https://zoro.to/demon-slayer-kimetsu-no-yaiba-swordsmith-village-arc-18056'
   },
@@ -128,7 +128,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",
     title: "Black Clover",
-    text: "This shows the love between  binod and Zendaya.",
+    text:"Asta and Yuno were abandoned at the same church on the same day",
     time: "TV.25 min",
     link: "https://zoro.to/black-clover-2404",
   },
@@ -136,7 +136,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/54/90/5490cb32786d4f7fef0f40d7266df532/5490cb32786d4f7fef0f40d7266df532.jpg",
     title: "One Piece",
-    text: "This shows the love between  binod and hinata.",
+    text: "Gold Roger was known as the Pirate King, the strongest..",
     time: "TV.23 min",
     link: "https://zoro.to/one-piece-100",
   },
@@ -145,15 +145,15 @@ const Sdata = [
       "https://img.zorores.com/_r/300x400/100/9c/bc/9cbcf87f54194742e7686119089478f8/9cbcf87f54194742e7686119089478f8.jpg",
 
     title: "Naruto:Shippuden",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "It has been two and a half years since Naruto Uzumaki left",
     time: "TV.16 min",
     link: "https://zoro.to/naruto-shippuden-355",
   },
   {
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",
-    title: "Demon Slayer: Kimetsu no ....",
-    text: "This shows the love between  binod and hinata.",
+    title: "Demon Slayer",
+    text:  "It adapts the story from volume 12 (chapter 98 onwards) of the manga series",
     time: "TV.30min",
     link:'https://zoro.to/demon-slayer-kimetsu-no-yaiba-swordsmith-village-arc-18056'
   },
@@ -161,7 +161,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",
     title: "Black Clover",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "Asta and Yuno were abandoned at the same church on the same day",
     time: "TV.25 min",
     link: "https://zoro.to/black-clover-2404",
   },
@@ -169,7 +169,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/54/90/5490cb32786d4f7fef0f40d7266df532/5490cb32786d4f7fef0f40d7266df532.jpg",
     title: "One Piece",
-    text: "This shows the love between  binod and hinata.",
+    text: "Gold Roger was known as the Pirate King, the strongest..",
     time: "TV.23 min",
     link: "https://zoro.to/one-piece-100",
   },
@@ -178,15 +178,15 @@ const Sdata = [
       "https://img.zorores.com/_r/300x400/100/9c/bc/9cbcf87f54194742e7686119089478f8/9cbcf87f54194742e7686119089478f8.jpg",
 
     title: "Naruto:Shippuden",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "It has been two and a half years since Naruto Uzumaki left",
     time: "TV.16 min",
     link: "https://zoro.to/naruto-shippuden-355",
   },
   {
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/db/2f/db2f3ce7b9cab7fdc160b005bffb899a/db2f3ce7b9cab7fdc160b005bffb899a.png",
-    title: "Demon Slayer: Kimetsu no ....",
-    text: "This shows the love between  binod and hinata.",
+    title: "Demon Slayer",
+    text:  "It adapts the story from volume 12 (chapter 98 onwards) of the manga series",
     time: "TV.30min",
     link:'https://zoro.to/demon-slayer-kimetsu-no-yaiba-swordsmith-village-arc-18056'
   },
@@ -194,7 +194,7 @@ const Sdata = [
     imgsrc:
       "https://img.zorores.com/_r/300x400/100/f5/8b/f58b0204c20ae3310f65ae7b8cb9987e/f58b0204c20ae3310f65ae7b8cb9987e.jpg",
     title: "Black Clover",
-    text: "This shows the love between  binod and Zendaya.",
+    text: "Asta and Yuno were abandoned at the same church on the same day",
     time: "TV.25 min",
     link: "https://zoro.to/black-clover-2404",
   },
